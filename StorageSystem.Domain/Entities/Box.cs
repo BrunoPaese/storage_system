ff5be100b5dd4a7624fa814d1d44c708636f584f
Bruno Paese <brunoviniciuspaese@gmail.com>
2024-06-03 21:33:47 -0300

    primeira implementacao dos objetos

---
+++ StorageSystem.Domain/Entities/Box.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageSystem.Domain.Entities;
+
+public class Box
+{
+    public Guid Id { get; set; }
+    public Product Product { get; set; }
+    public Adress Adress { get; set; }
+    public Status Status { get; set; }
+    public float Weigth { get; set; }
+}
---
+++ StorageSystem.Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageSystem.Domain.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public float Weight { get; set; }
+    public int PackagingQuantity { get; set; }
+}
+    
